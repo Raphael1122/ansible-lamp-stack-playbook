5c98a7ea9e8e11b48a8887e67f3d1fa69b0f3768
Raphael1122 <55252824+Raphael1122@users.noreply.github.com>
2020-05-22 16:11:49 -0400

    Update index.php

--- roles/apache2/files/index.php
+++ roles/apache2/files/index.php
@@ -1,5 +1,5 @@
 <img src="https://s3.amazonaws.com/media-p.slid.es/uploads/77212/images/1762786/ansible_logo_black_square.png" style="height: 100px;width: 200px;margin: 0 auto;display: table;"></img>
-<h1>Salut voici la page web de Raphael Corbin H2020</h1>
+<h1>Salut, voici la page web de Raphael Corbin H2020</h1>
 <?php
 	phpinfo();
 ?>
